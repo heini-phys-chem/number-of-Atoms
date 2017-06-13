8ccc375907ceb3ecfcadec045dd815422ba19b85
Heini <heini.phys.chem@gmail.com>
2017-06-13 15:47:50 +0200

    Update get_nAtoms.py

--- get_nAtoms.py
+++ get_nAtoms.py
@@ -15,7 +15,6 @@ def get_max_nAtom(atomNumber):
             nAtoms.append(Atom)
     return str(max(nAtoms))
 
-
 if __name__ == "__main__":
     dicAtoms = {1:'H', 6:'C', 7:'N', 8:'O', 9:'F', 15:'P', 16:'S', 17:'Cl', 35:'Br', 46:'Pd'}
 
