7504007aa69c3e216d9cac0323e5a6d5209c0f1b
Heini <heini.phys.chem@gmail.com>
2017-06-13 14:40:49 +0200

    Add files via upload

---
+++ get_nAtoms.py
@@ -0,0 +1,24 @@
+import sys
+import glob
+import openbabel
+import pybel
+
+
+def get_max_nAtom(atomNumber):
+    nAtoms = []
+    fin = glob.glob("*.xyz")
+    for f in fin:
+        Atom = 0
+        for mol in pybel.readfile("xyz", f):
+            for a in mol.atoms:
+                if int(a.OBAtom.GetAtomicNum()) == atomNumber:
+                    Atom +=1
+            nAtoms.append(Atom)
+    return str(max(nAtoms))
+
+
+if __name__ == "__main__":
+    dicAtoms = {1:'H', 6:'C', 7:'N', 8:'O', 9:'F', 15:'P', 16:'S', 17:'Cl', 35:'Br', 46:'Pd'}
+
+    for key, value in dicAtoms.iteritems():
+        print value + "\t" + get_max_nAtom(key) 
